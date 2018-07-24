79d9fe71387bdef46fe9530f5c55301fdd6ffe91
Sharmaine Requerme <srqrme@gmail.com>
2018-07-24 21:39:54 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,7 @@
 def unsafe?(speed)
-  if speed > 60 || speed < 40
+  if speed < 40 || speed > 60
     true
   else
     false
   end
-end
-
-
-
-def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false
 end
