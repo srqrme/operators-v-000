f838b7a7396790bd1161c490fa69187bc5d64d57
Sharmaine Requerme <srqrme@gmail.com>
2018-07-24 21:40:57 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,11 @@
 def unsafe?(speed)
-  if speed > 60 || speed < 40
+  if speed < 40 || speed > 60
     true
   else
     false
   end
 end
 
-
-
 def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false
+  speed < 40 || speed > 60 ? true : false
 end
